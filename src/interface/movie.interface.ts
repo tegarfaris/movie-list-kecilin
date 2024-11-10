3f76d31677bdfcd887a8b0ff010371d14deaf9fc
tegarfaris <tegarfarisn@gmail.com>
2024-11-10 16:37:53 +0700

    feat: add loading state and empty state

---
+++ src/interface/movie.interface.ts
@@ -0,0 +1,6 @@
+export interface IMovies {
+  poster_path: string;
+  original_title: string;
+  overview: string;
+  vote_average: string;
+}
--- src/page-modules/movie/_screens/movie-list/index.tsx
+++ src/page-modules/movie/_screens/movie-list/index.tsx
@@ -1,76 +1,58 @@
 /* eslint-disable @next/next/no-img-element */
-
-import React from "react";
+import SkeletonCard from "@/components/skeleton";
+import useMovies from "@/hooks/useMovies";
+import React, { useEffect, useState } from "react";
 
 const MovieList: React.FC = () => {
+  const { getListMovies, list, total } = useMovies();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    const delay = setTimeout(() => {
+      getListMovies();
+      setLoading(false);
+    }, 1000);
+    return () => clearTimeout(delay);
+  }, [getListMovies]);
+
+  if (loading) {
+    return (
+      <div className="grid grid-cols-5 gap-4">
+        {Array.from({ length: 5 }).map((_, idx) => (
+          <SkeletonCard key={idx} />
+        ))}
+      </div>
+    );
+  } else if (total === 0) {
+    return (
+      <div className="text-center py-20">
+        <p className="text-3xl font-semibold text-neutral-600">
+          No movies available
+        </p>
+        <p className="text-sm text-neutral-500">Please try again later.</p>
+      </div>
+    );
+  }
+
   return (
-    <div className="grid grid-cols-4 gap-4">
-      <div className="flex flex-col gap-4">
-        <img
-          src="https://awsimages.detik.net.id/community/media/visual/2024/08/24/the-crow-1_34.jpeg?w=700&q=90"
-          alt="https://awsimages.detik.net.id/community/media/visual/2024/08/24/the-crow-1_34.jpeg?w=700&q=90"
-          className="max-w-xs max-h-xs rounded-4 object-cover"
-        />
-
-        <p>Ini Judul</p>
-        <p>ini deskripsi</p>
-        <p>4.0</p>
-      </div>
-      <div className="flex flex-col gap-4">
-        <img
-          src="https://awsimages.detik.net.id/community/media/visual/2024/08/24/the-crow-1_34.jpeg?w=700&q=90"
-          alt="https://awsimages.detik.net.id/community/media/visual/2024/08/24/the-crow-1_34.jpeg?w=700&q=90"
-          className="max-w-xs max-h-xs rounded-4 object-cover"
-        />
-
-        <p>Ini Judul</p>
-        <p>ini deskripsi</p>
-        <p>4.0</p>
-      </div>
-      <div className="flex flex-col gap-4">
-        <img
-          src="https://awsimages.detik.net.id/community/media/visual/2024/08/24/the-crow-1_34.jpeg?w=700&q=90"
-          alt="https://awsimages.detik.net.id/community/media/visual/2024/08/24/the-crow-1_34.jpeg?w=700&q=90"
-          className="max-w-xs max-h-xs rounded-4 object-cover"
-        />
-
-        <p>Ini Judul</p>
-        <p>ini deskripsi</p>
-        <p>4.0</p>
-      </div>
-      <div className="flex flex-col gap-4">
-        <img
-          src="https://awsimages.detik.net.id/community/media/visual/2024/08/24/the-crow-1_34.jpeg?w=700&q=90"
-          alt="https://awsimages.detik.net.id/community/media/visual/2024/08/24/the-crow-1_34.jpeg?w=700&q=90"
-          className="max-w-xs max-h-xs rounded-4 object-cover"
-        />
-
-        <p>Ini Judul</p>
-        <p>ini deskripsi</p>
-        <p>4.0</p>
-      </div>
-      <div className="flex flex-col gap-4">
-        <img
-          src="https://awsimages.detik.net.id/community/media/visual/2024/08/24/the-crow-1_34.jpeg?w=700&q=90"
-          alt="https://awsimages.detik.net.id/community/media/visual/2024/08/24/the-crow-1_34.jpeg?w=700&q=90"
-          className="max-w-xs max-h-xs rounded-4 object-cover"
-        />
-
-        <p>Ini Judul</p>
-        <p>ini deskripsi</p>
-        <p>4.0</p>
-      </div>
-      <div className="flex flex-col gap-4">
-        <img
-          src="https://awsimages.detik.net.id/community/media/visual/2024/08/24/the-crow-1_34.jpeg?w=700&q=90"
-          alt="https://awsimages.detik.net.id/community/media/visual/2024/08/24/the-crow-1_34.jpeg?w=700&q=90"
-          className="max-w-xs max-h-xs rounded-4 object-cover"
-        />
-
-        <p>Ini Judul</p>
-        <p>ini deskripsi</p>
-        <p>4.0</p>
-      </div>
+    <div className="grid grid-cols-5 gap-4">
+      {list.map((movie, idx) => (
+        <div key={idx} className="flex flex-col gap-4 cursor-pointer">
+          <img
+            src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+            alt={movie.original_title || "Movie poster"}
+            className="max-w-xs h-[400px] rounded-lg object-cover"
+          />
+          <p className="line-clamp-2 text-2xl font-bold h-[60px]">
+            {movie.original_title}
+          </p>
+          <p className="line-clamp-4 text-sm text-neutral-500 text-justify">
+            {movie.overview}
+          </p>
+          <p className="text-sm text-neutral-500">tmdb: {movie.vote_average}</p>
+        </div>
+      ))}
     </div>
   );
 };
